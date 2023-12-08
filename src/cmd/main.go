7044b6ff538a987f26631bd4d0b023843f2c7d31
sabsarawut <sabsarawut@central.co.th>
2023-12-08 23:59:14 +0700

    [04]-function

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+	str, res := Multiply(10, 20)
+	fmt.Println(str, res)
+}
+
+func Concat(a, b string) string {
+	return a + b
+}
+
+func Multiply(a int, b int) (string, int) {
+	return "a x b", a * b
 }
