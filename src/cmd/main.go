af44d919bcde392665668f0380bc82bf798896c0
sabsarawut <sabsarawut@central.co.th>
2023-12-09 01:53:40 +0700

    [api]-http

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+}
+
+func loginHandler(w http.ResponseWriter, req *http.Request) {
+	method := req.Method
+	if method == "POST" {
+		jsonByte, err := io.ReadAll(req.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("invalid request"))
+			return
+		}
+		fmt.Println("body", string(jsonByte))
+		var user User
+		err = json.Unmarshal(jsonByte, &user)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("invalid request"))
+			return
+		}
+		fmt.Println("user", user.FirstName, user.Username)
+		w.WriteHeader(200)
+		w.Write(jsonByte)
+	}
+
+}
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/login", loginHandler)
+
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
 }
