cb49e5e5daee4fa867536619e780e3395dc72a5f
sabsarawut <sabsarawut@central.co.th>
2023-12-09 01:09:10 +0700

    [07]-struct

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type User struct {
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	FirstName string `json:"first_name"`
+	Age       int    `json:"age"`
+}
+
 func main() {
-	fmt.Println("Hello world")
+	var user User
+	// Dot notation
+	fmt.Println(user.Age)
+
+	user2 := User{Username: "user2", Password: "1234"}
+	fmt.Println(user2.Username)
+
 }
