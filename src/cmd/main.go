14016bc922f82fe3112b8f2915d992ff16a8b9b1
sabsarawut <sabsarawut@central.co.th>
2023-12-09 02:25:41 +0700

    [api]-gin

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	port := "8080"
+	engine := gin.Default()
+
+	engine.POST("/login", LoginHandler)
+
+	fmt.Println("Server started Port: ", port)
+	engine.Run(":8080")
+}
+func LoginHandler(c *gin.Context) {
+
+	c.JSON(200, c.Request.Body)
 }
