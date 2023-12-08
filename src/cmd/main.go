cc52bf52508413ae4fba34caeb0712b96ae26a98
sabsarawut <sabsarawut@central.co.th>
2023-12-09 01:02:50 +0700

    [06]-loop

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	for i := 0; i < 9; i++ {
+
+	}
+	x := 0
+	for x < 8 {
+		fmt.Println("a")
+	}
+	// for {
+
+	// } // infinite loop
+
+	var arr [9]string
+	for index, value := range arr {
+		fmt.Println(index, value)
+	}
+
 }
