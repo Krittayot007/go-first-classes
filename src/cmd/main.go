d3d6e1eb2547b2f7b36c8944b91d0f66dbdb86d5
sabsarawut <sabsarawut@central.co.th>
2023-12-09 01:23:30 +0700

    [10]-interface

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+type Human interface {
+	Eat(food string)
+	Walk(step string)
+	Sleep(hour int)
+}
+
+type Dog struct {
+	Name string
+}
+type Monkey struct {
+	Color string
+}
+
+type Man struct {
+	Hangout bool
+}
 
 func main() {
-	fmt.Println("Hello world")
+	// var x Dog
+	// var y Monkey
+	// var z Man
+	// Evolutions(x)
+	// Evolutions(y)
+	// Evolutions(z)
+
 }
+
+func Evolutions(life Human) {
+
+}
+
+func (m Monkey) Eat(fruit string)
+func (e Man) Eat(meet string)
+func (d Dog) Eat(vet string)
+func (m Monkey) Walk(s string)
+func (e Man) Walk(s string)
+func (j Monkey) Sleep(m int)
+func (e Man) Sleep(h int)
+func (e Man) Play(s string)
