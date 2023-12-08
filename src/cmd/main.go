61df631b91bf20f28e27fee10a92dc99f0d4dbf4
sabsarawut <sabsarawut@central.co.th>
2023-12-09 01:12:32 +0700

    [09]-reciever-function

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func Concat() {}
+
+type User struct {
+	Username string
+}
+
+func (u User) Concat2() {
+	fmt.Println("username: ", u.Username)
+}
+
 func main() {
+	var user User
 	fmt.Println("Hello world")
+	user.Concat2()
 }
