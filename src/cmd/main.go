8bcf9185cd943872493f20bc57010baddd74caca
sabsarawut <sabsarawut@central.co.th>
2023-12-09 00:56:18 +0700

    [08]-map

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+type Event struct {
+	E string
+}
+
 func main() {
-	fmt.Println("Hello world")
+	var obj1 map[string]interface{}
+	fmt.Println("obj1", obj1)
+
+	obj2 := make(map[string]interface{})
+	fmt.Println("obj2", obj2)
+
+	// obj1["value"] = 1
+	// obj2["value"] = 1
+
+	obj3 := make(map[int]interface{})
+	obj4 := make(map[bool]interface{})
+	obj5 := make(map[string]int)
+	obj6 := make(map[string]string)
+	obj7 := make(map[string]Event)
+
+	for key, value := range obj2 {
+		fmt.Println("key", key, "value", value)
+	}
+
+	fmt.Println(obj1, obj2, obj3, obj4, obj5, obj6, obj7)
+
 }
