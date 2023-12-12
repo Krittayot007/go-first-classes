36ccded17ea6abdb7eb781e051492628af4e1dbf
Krittayot007 <krittayot.som@gmail.com>
2023-12-12 13:36:43 +0700

    finish practice pointer

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -11,7 +11,7 @@ func main() {
 
 	var x int = 10
 	var y *int = &x
-	// var z int = *y -> ตัวนี้คืออะไร
+	// var z int = *y -> ตัวนี้คืออะไร -> กลับมาเป็น value ของ pointer นั้นๆ
 
 	fmt.Printf("%p = x\n%p = y value\n%p = y address \n", &x, y, &y)
 }
